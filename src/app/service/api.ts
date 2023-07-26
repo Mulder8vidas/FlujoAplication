79ee53e99222aacdca2282d88c57c54ffbb928d8
werty4777 <agalindoo@autonoma.edu.pe>
2023-07-25 23:44:54 -0500

    flujo finito cambio estrctura,pipes,queda pendiente cambio de formato flujo infinito revisar excel atte:adrian galindo fixx

--- src/app/service/api.ts
+++ src/app/service/api.ts
@@ -1,4 +1,4 @@
 export function ApiUrl(){
-  /*return "https://db08-54-207-101-15.ngrok-free.app";*/
-  return "http://localhost:8080";
+  return "https://db08-54-207-101-15.ngrok-free.app";
+  /*return "http://localhost:8080";*/
 }
